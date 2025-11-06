00ad479e4af83e8dd00af901d08598e8e9110af7
maryam <fatimaisherewithyou@gmail.com>
2025-11-06 10:58:58 +0330

    adding key Press

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-let input= document.querySelector('input')
-let button= document.querySelector('button')
-let ul= document.querySelector('ul')
+let input = document.querySelector('input')
+let button = document.querySelector('button')
+let ul = document.querySelector('ul')
 
-let addTask= (text) => {
-    let li= document.createElement('li')
-    li.textContent= text
+let createTask = (text) => {
+    let li = document.createElement('li')
+    li.textContent = text
     return li
 }
 // button.onclick= () => {
 //     let inputVal= input.value
-//     let realInput= addTask(inputVal)
+//     let realInput= createTask(inputVal)
 //     ul.appendChild(realInput)
 //     input.value ='';
 // }
-button.addEventListener('click', () => {
-    let inputVal= input.value
-    let realInput= addTask(inputVal)
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') addTask()
+    })
+
+let addTask= () => {
+    let inputVal = input.value.trim()
+    let realInput = createTask(inputVal)
     ul.appendChild(realInput)
-    input.value ='';
-})
+    input.value = '';
+}
+button.addEventListener('click', addTask)
